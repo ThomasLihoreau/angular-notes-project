9a19d0d0eb24fc5f28af86ff26cf97dc43fa6bc9
Thomas Lihoreau <thomas.lihoreau@epitech.eu>
2022-05-10 16:54:17 +0200

    Adding toolbar component

---
+++ src/app/app.toolbar.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+/**
+ * @title Basic toolbar
+ */
+@Component({
+  selector: "my-toolbar",
+  templateUrl: "app.toolbar.html",
+  styleUrls: ["app.toolbar.css"],
+})
+export class AppToolbar {}
